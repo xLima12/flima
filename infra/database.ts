c0990525dda5779d50842e07c20c3bd25fc633a3
xLima12 <90412987+xLima12@users.noreply.github.com>
2024-10-25 21:15:15 -0300

    feat: add `infra/database` and `infra/compose.yaml` structure more robust

--- infra/database.ts
+++ infra/database.ts
@@ -1,16 +1,24 @@
 import { Client } from "pg";
 
-async function query(queryObject: string) {
+interface QueryObject {
+  text: string;
+  values: string[];
+}
+
+async function query(queryObject: string | QueryObject) {
   let client;
   try {
     client = await getNewClient();
+    console.log(client);
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
     throw error;
   } finally {
-    await client.end();
+    if (client) {
+      await client.end();
+    }
   }
 }
 
@@ -19,7 +27,7 @@ async function getNewClient() {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
 
