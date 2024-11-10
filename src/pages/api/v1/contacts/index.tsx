74b84a57d71f5242d79b7c070f078da3c5c729b2
xLima12 <felipelima1910@gmail.com>
2024-11-10 00:42:57 -0300

    refactor: refactoring `pages/contacts` adjust allowed methods

--- src/pages/api/v1/contacts/index.tsx
+++ src/pages/api/v1/contacts/index.tsx
@@ -2,7 +2,7 @@ import database from "@/infra/database";
 import sendContactEmail from "@/src/services/emailService";
 
 export default async function handler(request, response) {
-  const allowedMethods = ["GET", "POST"];
+  const allowedMethods = "POST";
   if (!allowedMethods.includes(request.method)) {
     return response.status(405).json({
       error: `Method "${request.method}" not allowed`,
@@ -23,7 +23,7 @@ export default async function handler(request, response) {
 
       await sendContactEmail({ name, phone, email, subject, message });
 
-      response.status(200).json({ message: "Formulário enviado com sucesso!" });
+      response.status(200).json({ message: "Enviado com sucesso!" });
     } catch (error) {
       console.error("Erro ao processar o formulário:", error);
       response.status(500).json({ message: "Erro ao enviar o formulário" });
