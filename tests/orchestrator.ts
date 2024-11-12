536459acec106c1bda1a77609dd9309a4d0161bd
xLima12 <felipelima1910@gmail.com>
2024-11-12 00:27:51 -0300

    refactor: refactoring `orchestrator.ts`

--- tests/orchestrator.ts
+++ tests/orchestrator.ts
@@ -1,8 +1,9 @@
-/* eslint-disable no-unused-vars */
 import retry from "async-retry";
 import database from "@/infra/database";
 
 async function waitForAllServices() {
+  await waitForWebServer();
+
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
