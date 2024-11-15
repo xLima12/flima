d4116ba990547b3cab3a2304124c842f86a3fa68
xLima12 <felipelima1910@gmail.com>
2024-11-14 22:24:48 -0300

    refactor: add function for get ssl

--- infra/database.ts
+++ infra/database.ts
@@ -28,6 +28,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
 
   await client.connect();
@@ -40,3 +41,13 @@ const database = {
 };
 
 export default database;
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
